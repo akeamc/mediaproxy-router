6c7645600be31acfe38d18d20be25b6e66a0015e
Åke Amcoff <ake.amcoff@gmail.com>
2020-05-07 21:34:37 +0200

    nothing works

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,24 @@ use actix_web::{middleware, web, App, HttpResponse, HttpServer};
 use clap::Arg;
 use mediaproxy_common::query::Query;
 use url::Url;
+use serde::Deserialize;
 
 mod forwarder;
 
 fn router(query: web::Json<Query>, url: web::Data<Url>) -> HttpResponse {
     forwarder::forward(query.into_inner(), url.get_ref().clone())
+}
+
+#[derive(Deserialize)]
+pub struct FingerprintRequest {
+   fingerprint: String
+}
+
+fn fingerprint(web::Query(info): web::Query<FingerprintRequest>, url: web::Data<Url>) -> HttpResponse {
+    match Query::from_fingerprint(info.fingerprint) {
+        Ok(query) => forwarder::forward(query, url.get_ref().clone()),
+        Err(error) => actix_web::error::ErrorBadRequest(error).error_response()
+    }
 }
 
 #[actix_rt::main]
@@ -41,7 +54,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .data(web::JsonConfig::default().limit(4096))
             .data(forward_url.clone())
-            .service(web::resource("/").route(web::post().to(router)))
+            .service(web::resource("/").route(web::post().to(router)).route(web::get().to(fingerprint)))
     })
     .bind(listen_addr)?
     .run()
